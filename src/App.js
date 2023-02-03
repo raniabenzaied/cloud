50da696a000ebff92b0bb3fab783e82eb34110ad
raniabenzaied <benzaiedrania133@gmail.com>
2023-02-03 15:06:37 +0100

    google maps

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <iframe width="2000" height="1000" id="gmap_canvas" src="https://maps.google.com/maps?q=lac1 tunisia&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    </div>
+  );
+}
+
+export default App;
